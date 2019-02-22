de64de1e921cbbaced27603270ad1a1b4336d8f2
qwhat <justinmao@live.ca>
2019-02-22 23:35:12 +0000

    create views file for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,21 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resource :users do
+    root to: 'users#new'
+  end
+
+  resource :sessions, only: [:new, :create, :destroy] do
+    root to: 'sessions#new'
+  end
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:destroy]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
