a2667cbd746cb8e99c1248d1de577da29ceb0c9b
qwhat <justinmao@live.ca>
2019-02-27 02:06:23 +0000

    add specifications to the validations in the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :reviews
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 5}
+  validates :password_digest, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
